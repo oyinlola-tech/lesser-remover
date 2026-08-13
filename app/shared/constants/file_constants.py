611ca3aa6f7ea7b3904e875a66a7eee7cef09e21
Oluwayemi Oyinlola <oyinlola.tech@icloud.com>
2026-08-13 18:53:10 +0100

    feat: add shared: constants: file_constants

---
+++ app/shared/constants/file_constants.py
@@ -0,0 +1,23 @@
+MAX_FILENAME_LENGTH = 255
+DEFAULT_OUTPUT_FORMAT = "png"
+
+SUPPORTED_IMAGE_EXTENSIONS = {
+    ".jpg",
+    ".jpeg",
+    ".png",
+    ".webp",
+    ".bmp",
+    ".tiff",
+    ".tif",
+}
+
+SUPPORTED_PDF_EXTENSIONS = {
+    ".pdf",
+}
+
+SUPPORTED_ARCHIVE_EXTENSIONS = {
+    ".zip",
+    ".tar",
+    ".gz",
+    ".7z",
+}
